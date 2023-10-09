032b7078dc0437e583629c894251c34ba9ad2b16
Alien Brain <murad.dub@gmail.com>
2023-10-09 20:33:10 +0600

    Error/ Not Found page style fixed

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="flex flex-col gap-11 min-h-screen justify-center items-center bg-red-100">
-      <h2 className="text-4xl font-black text-red-700">Oops.. </h2>
+    <div className="flex flex-col gap-11 min-h-screen justify-center items-center bg-red-600">
+      <h2 className="text-4xl font-black text-white">Oops.. </h2>
       <h1 className="text-5xl font-bold">Page Not Found !!!</h1>
       <Link
         to={"/"}
